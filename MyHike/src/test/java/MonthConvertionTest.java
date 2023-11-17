1b30813025ac3995427905aef9ebc1de9f8ab107
michaelreinprecht <reinprecht.michael.business@gmail.com>
2023-11-17 09:14:39 +0100

    Tests for Month class

---
+++ MyHike/src/test/java/MonthConvertionTest.java
@@ -0,0 +1,20 @@
+import models.Month;
+import org.junit.*;
+
+public class MonthConvertionTest {
+    @Test
+    public void getMonthsByBitmap() {
+        String bitmap = "101010101010"; //Equal to January, March, May, Juli, September, November
+        String[] months = Month.getMonthsByBitmap(bitmap);
+        String[] expectedResult = new String[] { "January",  null ,"March", null, "May", null, "July", null, "September", null,"November", null};
+        Assert.assertEquals(months, expectedResult);
+    }
+
+    @Test
+    public void getBitmapFromMonths() {
+        String[] months = new String[] { "January",  null ,"March", null, "May", null, "July", null, "September", null,"November", null};
+        String expectedResult = "101010101010";
+        String bitmap = Month.getBitmapFromMonths(months);
+        Assert.assertEquals(bitmap, expectedResult);
+    }
+}
